1994403d35da625ebe9100a84452fedee5c9075f
Iury L. Murdock <iurylohan8@gmail.com>
2022-12-14 20:49:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function altura() {
+  var alt = document.getElementById("altura").value;
+  console.log(alt);
+
+  const spLargInst = document.querySelector("#alt").value:
+spLargInst.innerHTML = larg;
+
+  const box = document.querySelector("#box");
+  box.style.width = larg + "px";
+}
+
+function largura() {
+  var larg = document.getElementById("largura").value;
+  console.log(larg);
+  
+}
